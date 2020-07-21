37cef8086451ff55461bb205bf8133e4e016fe06
rodrigo <rodrigo.furtado@unilasalle.edu.br>
2020-07-21 01:03:22 -0300

    front

--- resources/views/layouts/app_pag.blade.php
+++ resources/views/layouts/app_pag.blade.php
@@ -177,7 +177,7 @@
                     <div class="pad">
                         <h1>
                             <a href="/" title="Plena Natura - Cosméticos Naturais">
-                                <img src="images/plena-natura-cosmeticos-naturais.jpg" alt="Plena Natura - Cosméticos Naturais">
+                                <img src="{{url('images/plena-natura-cosmeticos-naturais.jpg')}}" alt="Plena Natura - Cosméticos Naturais">
                             </a>
                         </h1>
 
